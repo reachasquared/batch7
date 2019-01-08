fbe6b301963e29aae3b1257c01f82f649a494df5
Sampath Namburi <sampath.namburi@oracle.com>
2019-01-08 09:59:01 -0500

    New files

---
+++ src/test/java/asquared/CodingBestPractice.java
@@ -0,0 +1,15 @@
+package asquared;
+
+import org.junit.Test;
+
+public class CodingBestPractice {//Class begins
+    @Test //JUnit annotation
+    public void codingBestPractice(){//codingBestPractice method begins
+        //business logic starts
+
+    }//codingBestPractice ends
+
+    public void anotherMethod(){//anotherMethod begins
+
+    }//anotherMethod ends
+}//Class ends
---
+++ src/test/java/asquared/Test_CodeRedundancy_2.java
@@ -0,0 +1,37 @@
+package asquared;
+
+import org.junit.Test;
+
+public class Test_CodeRedundancy_2 {
+    @Test
+    public void iAmLearning() {
+        System.out.print("I learned ");
+        System.out.println("2nd multiplication table");
+        System.out.println();
+
+        System.out.print("I learned ");
+        System.out.println("3rd multiplication " +
+                "table");
+        System.out.println();
+
+        System.out.print("I learned ");
+        System.out.println("4th multiplication table");
+        System.out.println();
+
+        System.out.print("I learned ");
+        System.out.println("5th multiplication table");
+        System.out.println();
+
+        System.out.print("I learned ");
+        System.out.println("6th multiplication table");
+        System.out.println();
+        System.out.print("I learned ");
+        System.out.println("7th multiplication table");
+        System.out.println();
+        System.out.print("I learned ");
+        System.out.println("8th multiplication table");
+        System.out.println();
+        System.out.print("I learned ");
+        System.out.println("9th multiplication table");
+    }
+}
