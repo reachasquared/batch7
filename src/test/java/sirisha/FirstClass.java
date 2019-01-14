e3a828b2682dea47bff2ba0abdb747c07d62cb49
Aditya Sirisha <aditya.4041@gmail.com>
2019-01-13 19:52:34 -0500

    My firstclass automation is complete

---
+++ src/test/java/sirisha/FirstClass.java
@@ -0,0 +1,18 @@
+package sirisha;
+
+import org.junit.Test;
+
+public class FirstClass { //Class begins
+    @Test
+    public void myFirstClass() {
+        System.out.println("First java program");
+
+
+
+    }
+
+
+
+
+
+}
