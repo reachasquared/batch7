c2f3b5a158a8ba028cc0fe79aa1a9f4911ca8b69
Aditya Sirisha <aditya.4041@gmail.com>
2019-01-13 19:57:09 -0500

    My firstclass automation is complete

---
+++ src/test/java/sirisha/Test_TrainedQA.java
@@ -0,0 +1,22 @@
+package sirisha;
+
+import org.junit.Test;
+
+public class Test_TrainedQA {
+    @Test
+    public void trainedQA(){
+        iLearned();
+       System.out.println("manual QA");
+       iLearned();
+       System.out.println("DB concepts");
+       iLearned();
+       System.out.println("Automation");
+
+    }
+
+    public void iLearned(){
+
+        System.out.print("At ASquared LLC I learned ");
+    }
+
+}
---
+++ src/test/java/sirisha/Test_VerifyLinks.java
@@ -0,0 +1,66 @@
+package sirisha;
+
+import core.engine.AsquaredWebDriverFactory;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+import java.util.concurrent.TimeUnit;
+
+import static org.junit.Assert.*;
+
+public class Test_VerifyLinks {
+    private WebDriver driver;
+    private String baseUrl;
+    private boolean acceptNextAlert = true;
+    private StringBuffer verificationErrors = new StringBuffer();
+
+    @Before//Junit Annotation
+    public void setUp() throws Exception {//setup method begins
+        //driver = new FirefoxDriver();
+        driver = AsquaredWebDriverFactory.getDriver();
+        //driver = new FirefoxDriver();
+        baseUrl = "http://newtours.demoaut.com/";
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+    }//setup method ends
+
+    @After
+    public void tearDown() throws Exception {
+        driver.quit();
+        String verificationErrorString = verificationErrors.toString();
+        if (!"".equals(verificationErrorString)) {
+            fail(verificationErrorString);
+        }
+    }
+
+
+    @Test
+    public void verifyLinks(){
+        // Go to http://newtours.demoaut.com/
+        driver.get(baseUrl);
+
+    // Verify page title is "Welcome: Mercury Tours"
+        assertEquals("Welcome: Mercury Tours",driver.getTitle());
+
+    // Verify Home link exist
+        assertTrue(driver.findElement(By.linkText("Home")).isDisplayed());
+
+    // Verify Flights link exist
+        assertTrue(driver.findElement(By.linkText("Flights")).isDisplayed());
+
+    // Verify Hotels link exist
+        assertTrue(driver.findElement(By.linkText("Hotels")).isDisplayed());
+
+    //Verify Car Rentals link exist
+        assertTrue(driver.findElement(By.linkText("Car Rentals")).isDisplayed());
+
+    // Verify Cruises link exist
+        assertTrue(driver.findElement(By.linkText("Cruises")).isDisplayed());
+
+        //Step 2	Click on SIGN-ON link in Header
+        driver.findElement(By.linkText("SIGN-ON")).click();
+    }
+}
+
