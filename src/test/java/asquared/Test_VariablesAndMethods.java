305da57746af7b00c030b10b705fe48f72877e29
Sampath Namburi <sampath.namburi@oracle.com>
2019-01-21 10:16:21 -0500

    Instantiating class

--- src/test/java/asquared/Test_VariablesAndMethods.java
+++ src/test/java/asquared/Test_VariablesAndMethods.java
@@ -11,7 +11,7 @@ public class Test_VariablesAndMethods {
     // character variable that can hold one character data
     char gender;
     // Integer variable that can hold an integer value
-    int age;
+    //int age;
     // String variable that can hold one string data
     //Variables can be declared and initiated at the same time like below
     String weekDay = "Sunday";
@@ -20,6 +20,7 @@ public class Test_VariablesAndMethods {
 
     @Test
     public void variablesAndMethods(){
+        int age;
         //Business logic using variables
         gender = 'f';
         age = 16;
@@ -27,22 +28,28 @@ public class Test_VariablesAndMethods {
         weekDay = "Sunday";
         status = false;
 
+
         //Display values
         displayValues();
+        System.out.println("age of my daughter: " + age);
 
         age = age + 10;
         weekDay = "Monday";
 
         displayValues();
+        System.out.println("age of my son : " + age);
     }
 
     public void displayValues(){
+        int age = 0;
         //displayValues
+        System.out.println();
+        System.out.println("Student Information");
         System.out.println("Gender: " + gender);//we are displaying label 'Gender' and value
-        System.out.println("age: " + age);
+        //System.out.println("age: " + age);
         System.out.println("Week: " + weekDay);
         System.out.println("Status:" + status);
 
-        System.out.println();
+
     }
 }
---
+++ src/test/java/classwork/Test_TrainedQA.java
@@ -0,0 +1,24 @@
+package classwork;
+
+import org.junit.Test;
+
+public class Test_TrainedQA {
+
+    @Test
+    public void trainedQA(){
+        iLearned();
+        System.out.println("manual QA");
+
+        iLearned();
+        System.out.println("DB concepts");
+
+        iLearned();
+        System.out.println("automation");
+    }
+
+    public void iLearned(){
+        System.out.print("At ASquared LLC I learned ");
+    }
+
+
+}
